bf6c973622666d75f9d8306e15afbd840590d12e
Eduardo Secco <dudssecco.code@gmail.com>
2024-11-06 18:41:36 -0300

    Saving on LocalStorage

--- tasklist/src/components/Main.js
+++ tasklist/src/components/Main.js
@@ -19,6 +19,22 @@ export default class Main extends Component{
     index: -1
   }
 
+  componentDidMount(){
+    const tasks = JSON.parse(localStorage.getItem('tasks'))
+
+    if(!tasks) return
+
+    this.setState({ tasks })
+  }
+
+  componentDidUpdate(prevProps, prevState){
+    const { tasks } = this.state
+
+    if(tasks === prevState.tasks) return
+
+    localStorage.setItem('tasks', JSON.stringify(tasks))
+  }
+
   handleChange = (e) => {
     this.setState({
       newTask: e.target.value
